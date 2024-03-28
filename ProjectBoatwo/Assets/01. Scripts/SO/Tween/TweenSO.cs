62c5470da02d07ceb13f57f9ceb31248da1da7f1
SEH00N <06psh0501@gmail.com>
2024-03-29 01:06:52 +0900

    tween so

---
+++ ProjectBoatwo/Assets/01. Scripts/SO/Tween/TweenSO.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public abstract class TweenSO : ScriptableObject
+{
+    [SerializeField] protected List<TweenParam> tweenParams = new List<TweenParam>();
+
+    private Sequence sequence = null;
+    protected Transform body = null;
+
+    public event Action OnTweenCompletedEvent = null;
+
+    public virtual void Init(Transform body)
+    {
+        this.body = body;
+    }
+
+	public void PlayTween(Action callback = null)
+    {
+        sequence = DOTween.Sequence();
+        sequence.OnComplete(HandleTweenCompleted);
+        sequence.OnComplete(() => callback?.Invoke());
+        OnTween(sequence);
+    }
+
+    public TweenParam GetParam(int index) => tweenParams[index];
+
+    protected virtual void HandleTweenCompleted()
+    {
+        OnTweenCompletedEvent?.Invoke();
+        sequence?.Kill();
+    }
+
+    protected abstract void OnTween(Sequence sequence);
+}
