67ba27c33a14e539464a450fcfca95fc9f6ce53d
JUNSUNG06 <forest2847@gmail.com>
2024-04-01 00:30:14 +0900

    modify skeleton accident

--- ProjectBoatwo/Assets/01. Scripts/System/Accident/Skeleton/Skeleton.cs
+++ ProjectBoatwo/Assets/01. Scripts/System/Accident/Skeleton/Skeleton.cs
@@ -1,9 +1,11 @@
 using System;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Skeleton : MonoBehaviour
 {
     [SerializeField] LayerMask groundLayer = 0;
+    [SerializeField] VisualEffect spawnEffect;
 
     public event Action<Skeleton> OnSkeletonDestroyEvent = null;
 
@@ -16,7 +18,11 @@ public class Skeleton : MonoBehaviour
     {
         Ray ray = new Ray(transform.position, Vector3.down);
         if(Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, groundLayer))
+        {
             transform.position = hit.point;
+            VisualEffect effect = Instantiate(spawnEffect, hit.point, Quaternion.identity, Ship.Instance.transform);
+            effect.Play();
+        }
     }
 
     private void OnDestroy()
