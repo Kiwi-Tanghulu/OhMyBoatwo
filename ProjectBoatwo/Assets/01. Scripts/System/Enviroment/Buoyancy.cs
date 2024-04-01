b8e339fc6e192250ddef9c6a81abbd6bd875b420
SEH00N <06psh0501@gmail.com>
2024-04-01 23:32:57 +0900

    asd

--- ProjectBoatwo/Assets/01. Scripts/System/Enviroment/Buoyancy.cs
+++ ProjectBoatwo/Assets/01. Scripts/System/Enviroment/Buoyancy.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.UIElements;
 
